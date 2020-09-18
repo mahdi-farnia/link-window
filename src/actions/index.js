7ddaaedeed85defe3986b5a7d06c70418ea20a40
mohammad mahdi <mohamadmahdi1383@outlook.com>
2020-09-18 13:59:27 +0430

    set up Redux

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+// Types Goes Here
+export const Action = {};
+
+// Functions Goes Here <--
+// Functions use to pass custom data to reducer with plain object:
+// () => ({ type: Actions.type1, customParam: 22 })
