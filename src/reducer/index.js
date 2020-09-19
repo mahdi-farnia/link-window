3c49a4270d92107397758a7a78609a265c422057
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2020-09-20 01:21:01 +0430

    fix reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,49 +1,22 @@
 import { combineReducers } from 'redux';
 import { Action } from '../actions';
 
-/**
- * Add New Url
- * @param {{url: string, name: string, visited: 0, dateCreated: number}[]} urls
- * @param {{ type: string, newUrl: { url: string, name: string, visited: 0, dateCreated: number } }} action
- * @returns {{url: string, name: string, visited: 0, dateCreated: number}[]}
- */
-const addUrl = (urls = [], { type, newUrl }) => {
-  if (type === Action.addUrl && typeof newUrl === 'object')
-    return [...urls, newUrl];
-
-  return urls;
-};
-
-/**
- * Remove old Url
- * @param {{url: string, name: string, visited: 0, dateCreated: number }[]} urls
- * @param {{ type: string, oldUrl: string }} action
- * @returns {{url: string, name: string, visited: 0, dateCreated: number }[]}
- */
-const removeUrl = (urls = [], { type, oldUrl }) => {
-  if (type === Action.removeUrl && typeof oldUrl === 'string')
-    return urls.filter((url) => url.url !== oldUrl);
-
-  return urls;
-};
-
-/**
- * Rename Url
- * @param {{url: string, name: string, visited: 0, dateCreated: number }[]} urls
- * @param {{ type: string, url: string, newName: string }} action
- * @returns {{url: string, name: string, visited: 0, dateCreated: number }[]}
- */
-const renameUrl = (urls = [], { type, url, newName }) => {
-  if (type === Action.renameUrl && typeof url === 'string')
-    return urls.map((urlObj) => {
-      if (urlObj.url === url) urlObj.name = newName;
-      return urlObj;
-    });
-  return urls;
+const url = (urlObjects = [], action) => {
+  switch (action.type) {
+    case Action.addUrl:
+      return [...urlObjects, action.newUrl];
+    case Action.removeUrl:
+      return urlObjects.filter((url) => url.url !== action.oldUrl);
+    case Action.renameUrl:
+      return urlObjects.map((urlObj) => {
+        if (urlObj.url === action.url) urlObj.name = action.newName;
+        return urlObj;
+      });
+    default:
+      return urlObjects;
+  }
 };
 
 export default combineReducers({
-  addUrl,
-  removeUrl,
-  renameUrl
+  url
 });
