2c4745e930db79345f4d9c7bed872593826ef6a6
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2020-09-20 21:46:40 +0430

    cover for close panel

--- src/components/LibraryPanel.js
+++ src/components/LibraryPanel.js
@@ -6,38 +6,42 @@ import LibraryItem from './LibraryItem';
 export default function LibraryPanel() {
   // @ts-ignore
   const links = useSelector((store) => store.links),
-    LibraryPanel = useRef(null);
+    LibraryPanel = useRef(null),
+    closePanel = () => LibraryPanel.current.classList.toggle('close');
 
   return (
-    <section ref={LibraryPanel} id="library-panel" className="close">
-      <button
-        id="close-library-panel"
-        title="Close Library Panel"
-        onClick={() => LibraryPanel.current.classList.toggle('close')}
-      >
-        <label>
-          <span className="row-1"></span>
-          <span className="row-2"></span>
-        </label>
-      </button>
-      <header className="library-panel__header">
-        <h2>Library</h2>
-      </header>
-      <div className="links-wrapper">
-        <ul id="links">
-          {Array.isArray(links) && links.length > 0 ? (
-            links
-              // Sort By Most Visited -> 5, 4, 3, 2, 1
-              .sort((a, b) => b.visited - a.visited)
-              .map((link) => (
-                <LibraryItem name={link.name} url={link.url} key={link.url} />
-              ))
-          ) : (
-            <span className="empty">No Links Found!</span>
-          )}
-        </ul>
-      </div>
-    </section>
+    <>
+      <section ref={LibraryPanel} id='library-panel' className='close'>
+        <button
+          id='close-library-panel'
+          title='Close Library Panel'
+          onClick={closePanel}
+        >
+          <label>
+            <span className='row-1'></span>
+            <span className='row-2'></span>
+          </label>
+        </button>
+        <header className='library-panel__header'>
+          <h2>Library</h2>
+        </header>
+        <div className='links-wrapper'>
+          <ul id='links'>
+            {Array.isArray(links) && links.length > 0 ? (
+              links
+                // Sort By Most Visited -> 5, 4, 3, 2, 1
+                .sort((a, b) => b.visited - a.visited)
+                .map((link) => (
+                  <LibraryItem name={link.name} url={link.url} key={link.url} />
+                ))
+            ) : (
+              <span className='empty'>No Links Found!</span>
+            )}
+          </ul>
+        </div>
+      </section>
+      <div className='library-panel__cover' onClick={closePanel}></div>
+    </>
   );
 }
 
