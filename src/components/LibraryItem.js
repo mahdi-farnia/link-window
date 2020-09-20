dab8234bb84d720e3c1d89f3b57ae7e7d0f8c2ea
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2020-09-20 23:38:36 +0330

    rename links in store added

--- src/components/LibraryItem.js
+++ src/components/LibraryItem.js
@@ -1,13 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { renameLink, removeFromLibrary } from '../actions';
 import Favicon from './LibraryItemFavicon';
+
+const classLists = {
+  renameLinkFocused: 'rename-link-focused'
+};
 
 export default function LibraryItem({ name, url }) {
   // Use Loading Favicon First
-  const [icon, setIcon] = useState({ src: '', isLoading: true }),
+  const itemEl = useRef(null),
+    linkEl = useRef(null),
+    [icon, setIcon] = useState({ src: '', isLoading: true }),
     // If Image Not Loaded Use Name Instead
     textIcon = name[0] + name[1],
     redirectUrl = /https:\/\/|http:\/\//.exec(url + '') ? url : 'http://' + url,
-    requestUrl = new URL(redirectUrl).hostname;
+    requestUrl = new URL(redirectUrl).hostname,
+    dispatch = useDispatch();
 
   useEffect(() => {
     (async () => {
@@ -31,19 +40,48 @@ export default function LibraryItem({ name, url }) {
   }, [requestUrl]);
 
   return (
-    <li title={name || 'NO NAME'}>
+    <li title={name || 'NO NAME'} ref={itemEl}>
       <Favicon icon={icon} textIcon={textIcon} />
       <a
         href={redirectUrl}
         target='_blank'
         rel='noopener noreferrer'
         className='url-name'
+        ref={linkEl}
       >
         {name || 'NO NAME'}
       </a>
       <div className='link-item__option'>
-        <span className='rename' title='Rename Link'></span>
-        <span className='delete' title='Delete Link'></span>
+        <span
+          className='rename'
+          title='Rename Link'
+          onClick={() => {
+            linkEl.current.contentEditable = true;
+            linkEl.current.focus();
+            // Selects Content
+            const range = document.createRange(),
+              select = window.getSelection();
+            range.selectNodeContents(linkEl.current);
+            select.removeAllRanges();
+            select.addRange(range);
+            //
+            itemEl.current.classList.add(classLists.renameLinkFocused);
+            linkEl.current.addEventListener('keydown', function (e) {
+              if (e.which === 13 || e.key === 'Enter') {
+                e.preventDefault();
+                this.blur();
+              }
+            });
+            linkEl.current.addEventListener('blur', () => {
+              itemEl.current.classList.remove(classLists.renameLinkFocused);
+              linkEl.current.contentEditable = false;
+              dispatch(
+                renameLink(linkEl.current.href, linkEl.current.textContent)
+              );
+            });
+          }}
+        ></span>
+        <span className='delete' title='Delete Link' onClick={() => {}}></span>
       </div>
     </li>
   );
--- src/components/LibraryItemFavicon.js
+++ src/components/LibraryItemFavicon.js
@@ -24,3 +24,5 @@ export default function LibraryItemFavicon({ icon, textIcon }) {
     </>
   );
 }
+
+React.memo(LibraryItemFavicon);
